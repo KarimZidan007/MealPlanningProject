b180a84aeea39fbcfa098e1d140a3c046348a58a
Karimzidan007 <karimzidan7474@gmail.com>
2024-09-23 17:33:51 +0300

    Sign Up UI has been added

--- AndriodProject/app/src/main/java/com/example/sidechefproject/login/MainActivity.java
+++ AndriodProject/app/src/main/java/com/example/sidechefproject/login/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sidechefproject.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +10,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.sidechefproject.R;
 
 public class MainActivity extends AppCompatActivity {
-
+Button createAccount;
+Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Intent createAccountOut = new Intent(this ,createAccount.class);
+        startActivity(createAccountOut);
     }
 }
